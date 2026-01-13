2decda9d3964f39bb4b3d362f7234be81ce888d0
sumitnautiyal001 <sumit.nautiyal@ufaber.com>
2026-01-13 17:31:36 +0530

    static files changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('api/', include('accounts.urls')),
     path('api/', include('products.urls')),
     path('api/', include('orders.urls'))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
