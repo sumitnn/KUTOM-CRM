76bb5922bb89aef6167dff4e1d6c71007bf1c8be
sumitnautiyal001 <sumit.nautiyal@ufaber.com>
2026-02-27 16:31:28 +0530

    cahnges

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1450,7 +1450,7 @@ def get_admin_email(request):
 
 @api_view(['GET'])
 def get_api_status(request):
-    return Response({"message": "Api Working Properly. First 2"})
+    return Response({"message": "Api Working Properly. First 3"})
 
 class NewUserApplicationListView(APIView):
     permission_classes = [IsAdminRole]
